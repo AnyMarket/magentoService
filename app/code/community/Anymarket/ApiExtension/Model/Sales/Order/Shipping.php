ac9003c10d5379dcd7131567377fcf95dee6d3f4
jose.junior <!@#Mudar5>
2018-11-26 12:14:13 -0200

    [ANYMARKET] - GET SHIPPING OPTIONS

--- app/code/community/Anymarket/ApiExtension/Model/Sales/Order/Shipping.php
+++ app/code/community/Anymarket/ApiExtension/Model/Sales/Order/Shipping.php
@@ -2,14 +2,23 @@
 
 class Anymarket_ApiExtension_Model_Sales_Order_Shipping extends Mage_Catalog_Model_Api_Resource
 {
-    
+
     public function getAllShippingMethods()
     {
         $carriers = Mage::getsingleton("shipping/config")->getAllCarriers();
         $carriersData = array();
-        foreach($carriers as $code => $method){
-            $carrierTitle = Mage::getStoreConfig("carriers/$code/title");
-            $carriersData[] = array('value'=>$code,'label'=>$carrierTitle);
+        foreach ($carriers as $carrierCode => $carrierModel) {
+            $carrierTitle = Mage::getStoreConfig("carriers/$carrierCode/title");
+
+            $options = array();
+            if ($carrierMethods = $carrierModel->getAllowedMethods()) {
+                foreach ($carrierMethods as $methodCode => $method) {
+                    $code = $carrierCode . '_' . $methodCode;
+                    $options[] = array('value' => $code, 'label' => $method);
+                }
+            }
+
+            $carriersData[] = array('value' => $carrierCode, 'label' => $carrierTitle, 'options' => $options);
         }
         return $carriersData;
     }
