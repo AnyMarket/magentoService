07001bc171a5084dfab2dfd76cd1c1a655053e0c
Jose <jose>
2019-03-18 11:28:59 -0300

    [ANYMARKET] - Melhorias para Observer

--- app/code/community/Anymarket/ApiExtension/Model/Observer.php
+++ app/code/community/Anymarket/ApiExtension/Model/Observer.php
@@ -12,8 +12,15 @@ class Anymarket_ApiExtension_Model_Observer
         $modelFeed->save();
     }
 
-    private function doCallAnymarket($host)
+    private function doCallAnymarket($host, $oi, $storeCode, $itemId)
     {
+        $body = array(
+            "oi" => $oi,
+            "version" => "MAGENTO_1",
+            "storeCode" => $storeCode,
+            "idItem" => $itemId
+        );
+
         $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_URL => $host,
@@ -23,7 +30,7 @@ class Anymarket_ApiExtension_Model_Observer
             CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "",
+            CURLOPT_POSTFIELDS => json_encode($body),
             CURLOPT_HTTPHEADER => array(
                 "cache-control: no-cache",
                 "content-type: application/json"
@@ -39,31 +46,6 @@ class Anymarket_ApiExtension_Model_Observer
         }
     }
 
-    /**
-     * @param $observer
-     *
-     * @return $this
-     */
-    public function updateOrderAnyMarketObs($observer)
-    {
-        if ($observer->getEvent()->getOrder()) {
-            $storeID = $observer->getEvent()->getOrder()->getStoreId();
-            $OrderID = $observer->getEvent()->getOrder()->getIncrementId();
-            $order = new Mage_Sales_Model_Order();
-            $order->loadByIncrementId($OrderID);
-
-            $oi = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_oi_field', $storeID);
-            $feed = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_using_feed_order_field', $storeID);
-            if ($feed == "1") {
-                $this->sendToFeed($OrderID, "1", $oi);
-            } else {
-                $host = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_host_field', $storeID);
-                $host = $host . "/public/api/anymarketcallback/order/" . $oi . "/MAGENTO_1/" . $storeID . "/" . $OrderID;
-                $this->doCallAnymarket($host);
-            }
-
-        }
-    }
 
     private function isNewOrder($order)
     {
@@ -79,7 +61,30 @@ class Anymarket_ApiExtension_Model_Observer
 
     /**
      * @param $observer
-     * @return array
+     */
+    public function updateOrderAnyMarketObs($observer)
+    {
+        if ($observer->getEvent()->getOrder()) {
+            $storeID = $observer->getEvent()->getOrder()->getStoreId();
+            $orderID = $observer->getEvent()->getOrder()->getIncrementId();
+            $order = new Mage_Sales_Model_Order();
+            $order->loadByIncrementId($orderID);
+
+            $oi = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_oi_field', $storeID);
+            $feed = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_using_feed_order_field', $storeID);
+            if ($feed == "1") {
+                $this->sendToFeed($orderID, "1", $oi);
+            } else {
+                $host = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_host_field', $storeID);
+                $host = $host . "/public/api/anymarketcallback/order";
+                $this->doCallAnymarket($host, $oi, $storeID, $orderID);
+            }
+
+        }
+    }
+
+    /**
+     * @param $observer
      */
     public function sendProdAnyMarket($observer)
     {
@@ -95,15 +100,14 @@ class Anymarket_ApiExtension_Model_Observer
                 $this->sendToFeed($product->getSku(), "2", $oi);
             } else {
                 $host = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_host_field', $storeID);
-                $host = $host . "/public/api/anymarketcallback/product/" . $oi . "/MAGENTO_1/" . $storeID . "/" . $product->getSku();
-                $this->doCallAnymarket($host);
+                $host = $host . "/public/api/anymarketcallback/product";
+                $this->doCallAnymarket($host, $oi, $storeID, $product->getSku());
             }
         }
     }
 
     /**
      * @param $observer
-     * @return array
      */
     public function catalogInventorySave($observer)
     {
@@ -120,8 +124,8 @@ class Anymarket_ApiExtension_Model_Observer
             if ($feed == "1") {
                 $this->sendToFeed($product->getSku(), "0", $oi);
             } else {
-                $host = $host . "/public/api/anymarketcallback/stockPrice/" . $oi . "/" . $product->getSku();
-                $this->doCallAnymarket($host);
+                $host = $host . "/public/api/anymarketcallback/stockPrice";
+                $this->doCallAnymarket($host, $oi, "", $product->getSku());
             }
         } else {
             foreach ($product->getStoreIds() as $storeID) {
@@ -132,8 +136,8 @@ class Anymarket_ApiExtension_Model_Observer
                     if ($feed == "1") {
                         $this->sendToFeed($product->getSku(), "0", $oi);
                     } else {
-                        $host = $host . "/public/api/anymarketcallback/stockPrice/" . $oi . "/" . $product->getSku();
-                        $this->doCallAnymarket($host);
+                        $host = $host . "/public/api/anymarketcallback/stockPrice";
+                        $this->doCallAnymarket($host, $oi, "", $product->getSku());
                     }
                 }
             }
